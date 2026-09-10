027833be7a10a44af0a5d03f170e75c613b3ed41
unknown <ngoan1131990@gmail.com>
2026-09-10 19:57:11 +0700

    add pwm

--- Lesson/Exercise/Code/Timer0_PWM/Timer0_PWM.ino
+++ Lesson/Exercise/Code/Timer0_PWM/Timer0_PWM.ino
@@ -1,12 +1,4 @@
-// void setup() {
-//   // put your setup code here, to run once:
 
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-
-// }
 
 
 //Basic code
@@ -52,37 +44,79 @@
 
 
 //Safe code
-#define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-#include <stdint.h>
+// #define F_CPU 16000000UL
+// #include <avr/io.h>
+// #include <util/delay.h>
+// #include <avr/interrupt.h>
+// #include <stdint.h>
 
-volatile uint32_t my_millis = 0;
-uint32_t time = 0;
-ISR(TIMER0_COMPA_vect) {
-  my_millis++;
-}
+// volatile uint32_t my_millis = 0;
+// uint32_t time = 0;
+// ISR(TIMER0_COMPA_vect) {
+//   my_millis++;
+// }
 
-uint32_t read_time(void){
-  cli();
-  uint32_t mil;
-  mil = my_millis;
-  sei();
-  return mil;
-}
+// uint32_t read_time(void){
+//   cli();
+//   uint32_t mil;
+//   mil = my_millis;
+//   sei();
+//   return mil;
+// }
+
+// int main(void) {
+//   //Config
+//   //Timer 0 config
+//   TCCR0A |= (1 << WGM01);
+//   TCCR0B |= (1 << CS01) | (1 << CS00);
+//   OCR0A = 249;
+//   TIMSK0 |= (1 << OCIE0A);
+
+//   //Output config
+//   PORTB &= ~0X01;
+//   DDRB |= 0X01;
+
+
+
+//   // //Interrupt enable
+//   // sei();
+
+//   //init variable
+//   uint32_t ms;
+//   //main loop
+//   while (1) {
+//     ms = read_time();
+
+//     if(ms - time >= 500){
+//       PINB = 1;
+//       time = ms;
+//     }
+
+//   }
+// }
+
+
+//PWM
+// #define F_CPU 16000000UL
+// #include <avr/io.h>
+// #include <util/delay.h>
+// #include <avr/interrupt.h>
+// #include <stdint.h>
+
+
+
 
 int main(void) {
   //Config
   //Timer 0 config
-  TCCR0A |= (1 << WGM01);
+  TCCR0A |= (1 << WGM01)|(1<<WGM00)|(1<<COM0B1);
   TCCR0B |= (1 << CS01) | (1 << CS00);
-  OCR0A = 249;
-  TIMSK0 |= (1 << OCIE0A);
+  // OCR0A = 249;
+  // TIMSK0 |= (1 << OCIE0A);
 
   //Output config
-  PORTB &= ~0X01;
-  DDRB |= 0X01;
+  PORTD &= ~(1<<PORTD5);
+  DDRD |= 0XFF;
 
 
 
@@ -90,16 +124,10 @@ int main(void) {
   // sei();
 
   //init variable
-  uint32_t ms;
   //main loop
+  OCR0B = 200;
   while (1) {
-    ms = read_time();
-
-    if(ms - time >= 500){
-      PINB = 1;
-      time = ms;
-    }
-
+    
   }
 }
 
